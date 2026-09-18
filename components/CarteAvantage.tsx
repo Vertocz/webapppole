ae81ace2fd587df7287a76d1d1224e0bebc78b61
Vertocz <victor_creze@hotmail.com>
2026-09-18 18:33:06 +0200

    cartes avantage 6

--- components/CarteAvantage.tsx
+++ components/CarteAvantage.tsx
@@ -5,7 +5,7 @@ import { supabase } from "@/lib/supabase";
 import Card from "./Card";
 
 const STORAGE_BASE =
-  "https://fxvotvtapcwzvjhfreqv.supabase.co/storage/v1/object/public/Cartes/";
+  "https://fxvotvtapcwzvjhfreqv.supabase.co/storage/v1/object/public/cartes/";
 
 interface CarteRow {
   id: number;
